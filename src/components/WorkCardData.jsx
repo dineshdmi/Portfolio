e12a37ce531a7c26847dcd2b9df785d8de87ae40
dineshdmi <mdinu8127@gmail.com>
2024-06-28 13:11:24 +0530

    changes

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -14,15 +14,15 @@ const ProjectCardData = [
     imgsrc: pro2,
     title: "Car Rent",
     text: "Car rent service build with react fully responsive.",
-    view: "https://carrentsidhaman.netlify.app/home",
+    // view: "https://carrentsidhaman.netlify.app/home",
     source: "https://github.com/dineshdmi/Car_Rent",
   },
   {
     imgsrc: pro1,
     title: "Mizoram Library Management",
     text: "Library Management System.",
-    view: "https://boodmo.netlify.app/",
-    source: "https://github.com/dineshdmi/Boodmo",
+    // view: "/",
+    source: "https://github.com/dineshdmi/Mizoram",
   },
 ];
 
